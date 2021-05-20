4b69d025aec8e4ec02d9be7adfb2741c2b64ac84
AnatolyKul <84135820+AnatolyKul@users.noreply.github.com>
2021-05-20 16:49:54 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Plane Boing = new Plane ("Boing", "White", "EFA - 123", "750 km/h");
+            Boing.name("Boing");
+            Boing.color("White");
+            Boing.model("EFA - 123");
+            Boing.speed("750 km/h");
+            Boing.fly(Boing);
+            Boing.land(Boing);
+        }
+    }
+}
